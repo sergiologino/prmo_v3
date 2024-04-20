ea98f3ea3168c5fd3c05a95f3e75790f24449ecc
goosLy1 <gray.goosly@gmail.com>
2024-04-20 13:06:45 +0300

    Change encoding

--- prmo/src/main/kotlin/ru/prmo/dto/OperationRecordDto.kt
+++ prmo/src/main/kotlin/ru/prmo/dto/OperationRecordDto.kt
@@ -4,6 +4,6 @@ import jakarta.validation.constraints.Digits
 
 data class OperationRecordDto(
     var operationName: String? = null,
-    @field:Digits(message = "Похоже, вы ввели вместо числа символ или отрицательное число", integer = 100, fraction = 0)
+    @field:Digits(message = "РџРѕС…РѕР¶Рµ, РІС‹ РІРІРµР»Рё РІРјРµСЃС‚Рѕ С‡РёСЃР»Р° СЃРёРјРІРѕР» РёР»Рё РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРµ С‡РёСЃР»Рѕ", integer = 100, fraction = 0)
     var count: Int? = null
 )
